49bb9cc5116e5c01e58f1cc4bc8ff87ff833216a
easonycliu <easonliu@ucla.edu>
2025-04-16 00:46:02 -0700

    change tpc-h input

--- tools/example_input.cpp
+++ tools/example_input.cpp
@@ -603,36 +603,36 @@ int main(int argc, char** argv) {
 	} else if (problem_name == "tpc_h_q4") {
 		for (std::uint64_t i = 0; i != input_size; i++) {
 			std::uint64_t blocked_party = get_blocked_worker(i, num_workers, input_size);
-			garbler_writers[blocked_party]->write32(i);
-			garbler_writers[blocked_party]->write32(1);
+			garbler_writers[blocked_party]->write32(0);
+			garbler_writers[blocked_party]->write32(0);
 		}
 
 		for (std::uint64_t i = 0; i != input_size; i++) {
 			std::uint64_t blocked_party = get_blocked_worker(i, num_workers, input_size);
-			evaluator_writers[blocked_party]->write32(i);
+			evaluator_writers[blocked_party]->write32(0);
 		}
 	} else if (problem_name == "tpc_h_q8") {
 		for (std::uint64_t i = 0; i != input_size; i++) {
 			std::uint64_t blocked_party = get_blocked_worker(i, num_workers, input_size);
-			garbler_writers[blocked_party]->write32(i * 8);
-			garbler_writers[blocked_party]->write32(i * 8 + 1);
-			garbler_writers[blocked_party]->write32(i * 8 + 2);
-			garbler_writers[blocked_party]->write32(i * 8 + 3);
-			garbler_writers[blocked_party]->write32(i * 8 + 4);
-			garbler_writers[blocked_party]->write32(i * 8 + 5);
-			garbler_writers[blocked_party]->write32(i * 8 + 6);
-			garbler_writers[blocked_party]->write32(i * 8 + 7);
+			garbler_writers[blocked_party]->write32(0);
+			garbler_writers[blocked_party]->write32(0);
+			garbler_writers[blocked_party]->write32(0);
+			garbler_writers[blocked_party]->write32(0);
+			garbler_writers[blocked_party]->write32(0);
+			garbler_writers[blocked_party]->write32(0);
+			garbler_writers[blocked_party]->write32(0);
+			garbler_writers[blocked_party]->write32(0);
 		}
 		for (std::uint64_t i = 0; i != input_size; i++) {
 			std::uint64_t blocked_party = get_blocked_worker(i, num_workers, input_size);
-			evaluator_writers[blocked_party]->write32(i * 8 + input_size * 8);
-			evaluator_writers[blocked_party]->write32(i * 8 + input_size * 8 + 1);
-			evaluator_writers[blocked_party]->write32(i * 8 + input_size * 8 + 2);
-			evaluator_writers[blocked_party]->write32(i * 8 + input_size * 8 + 3);
-			evaluator_writers[blocked_party]->write32(i * 8 + input_size * 8 + 4);
-			evaluator_writers[blocked_party]->write32(i * 8 + input_size * 8 + 5);
-			evaluator_writers[blocked_party]->write32(i * 8 + input_size * 8 + 6);
-			evaluator_writers[blocked_party]->write32(i * 8 + input_size * 8 + 7);
+			evaluator_writers[blocked_party]->write32(0);
+			evaluator_writers[blocked_party]->write32(0);
+			evaluator_writers[blocked_party]->write32(0);
+			evaluator_writers[blocked_party]->write32(0);
+			evaluator_writers[blocked_party]->write32(0);
+			evaluator_writers[blocked_party]->write32(0);
+			evaluator_writers[blocked_party]->write32(0);
+			evaluator_writers[blocked_party]->write32(0);
 		}
 	} else if (problem_name == "mpspdz_matrix_multiply" || problem_name == "pmpspdz_matrix_multiply") {
 		std::uint8_t log_num_workers = mage::util::log_base_2(num_workers);
